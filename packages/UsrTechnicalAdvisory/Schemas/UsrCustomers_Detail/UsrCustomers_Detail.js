5cf4d4c6ecea672e98090e8d9ed40da3e8618ef7
Oleg Drobina <oleg.drobina.job@ukr.net>
2026-02-26 15:57:58 +0200

    The initial commit

---
+++ packages/UsrTechnicalAdvisory/Schemas/UsrCustomers_Detail/UsrCustomers_Detail.js
@@ -0,0 +1,8 @@
+define('UsrCustomers_Detail', [], function () {
+	return {
+		entitySchemaName: 'UsrCustomers',
+		details: /**SCHEMA_DETAILS*/ {} /**SCHEMA_DETAILS*/,
+		diff: /**SCHEMA_DIFF*/ [] /**SCHEMA_DIFF*/,
+		methods: {},
+	};
+});
